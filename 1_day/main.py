0a83fd73b5c3d6cf6f9f3c269da3d8300ef15707
junyoung <junyoung>
2025-04-18 17:57:33 +0900

    clear day1

---
+++ 1_day/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator!")
+
+city_name = input("What's the name og the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print(f"Your band name could be {city_name} {pet_name}")
